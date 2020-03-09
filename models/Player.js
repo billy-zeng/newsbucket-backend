a99edd57098a5ab89bd256ae137d97fad8c18c09
billy-zeng <bzeng95@gmail.com>
2020-03-09 11:07:31 -0700

    builds Player model

---
+++ models/Player.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const PlayerSchema = mongoose.Schema({
+  name: String,
+  age: Number,
+  height: String,
+  weight: Number,
+  image: String,
+  teams: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Team'
+  }]
+});
+
+const Player = mongoose.model('Player', PlayerSchema);
+
+module.exports = Player;
