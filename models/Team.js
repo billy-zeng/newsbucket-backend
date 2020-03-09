b7d1fb32f755c506fe277eb3ffa9bc2172b945bb
billy-zeng <bzeng95@gmail.com>
2020-03-09 11:05:41 -0700

    builds Team model

---
+++ models/Team.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const TeamSchema = mongoose.Schema({
+  name: String,
+  yearFounded: String,
+  allTimeRecord: String,
+  logo: String,
+  players: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Player'
+  }]
+});
+
+const Team = mongoose.model('Team', TeamSchema);
+
+module.exports = Team;
