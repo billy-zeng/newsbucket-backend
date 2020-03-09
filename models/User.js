62a22edd1730b0be253458b46c430d7903d70f85
billy-zeng <bzeng95@gmail.com>
2020-03-09 10:52:21 -0700

    builds user model

---
+++ models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema({
+  username: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
